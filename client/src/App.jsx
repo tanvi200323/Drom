d3cbb7b3eb00733fba26e1aeb92c2e9271598094
tanvi200323 <113408281+tanvi200323@users.noreply.github.com>
2024-12-11 13:38:57 +0530

    First commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
-  import React from 'react'
+  import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import About from "./pages/About"
+import { Profile } from "./pages/Profile"
+import SignUp from "./pages/SignUp"
+import Header from "./components/Header"
 
   const App = () => {
     return (
-      <div><h1>Hello</h1></div>
+      <BrowserRouter>
+      <Header/>
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        
+    </Routes>
+    </BrowserRouter>
     )
   }
 
